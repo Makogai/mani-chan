982807199e5581cc1cb32f83a151b41476795fc0
Marko <mare.sampbn@gmail.com>
2021-02-02 10:12:13 +0100

    Down token entry

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -52,6 +52,19 @@
 
 </body>
 <script>
+    // Get the input field
+var input = document.getElementById("hero-field");
+
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("go").click();
+  }
+});
     $('#go').click(function(){
     let token = $('#hero-field').val();
     location.href='/'+ token;
